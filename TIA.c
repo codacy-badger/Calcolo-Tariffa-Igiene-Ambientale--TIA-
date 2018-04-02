316e8d73d79ff9be5e75655a0f4f9f2053e3c89d
Francesco Masala <francescomasala270202@gmail.com>
2018-04-02 17:05:57 +0200

    Added Things
    Added Urban & ExtraUrban Territory

--- TIA.c
+++ TIA.c
@@ -1,4 +1,4 @@
-//  main.c
+//  TIA.c
 //  Calcolo della Tariffa Igiene Ambientale (TIA)
 //  Versione 0.1
 //
@@ -11,15 +11,36 @@
 int main() {
             int comp=0;
             int qfmq=0;
+            int tariffa=0;
     printf("Calcolo per la Tariffia Igiene Ambientale\nVersione 0.1\nFrancesco Masala\n");
+    printf("Selezionare il territorio\nTerritorio Urbano=1\nTerritorio ExtraUrbano=2");
+    scanf("%d", &tariffa);
 
-        printf("inserire il numero di componenti della famiglia:\n");
-        strtod("%d",comp);
+        if (tariffa == 2) {
+            printf("Hai selezionato Territorio Extraurbano\n");
+        } else {
+            printf("Hai selezionato Territorio Extraurbano\n");
+            printf("inserire il numero di componenti della famiglia:\n");
+            scanf("%d", &comp);
 
-        printf("inserire il numero di metri quadrati della casa:\n");
-        strtod("%d",qfmq);
+            printf("inserire il numero di metri quadrati della casa:\n");
+
+            scanf("%d", &qfmq);
+
+            printf("Resoconto totale:\nComponenti della famiglia: %d\nMetri quadrati della casa:%d\n",comp,qfmq);
+        }
+        if (tariffa == 1) {
+        } else {
+            printf("Hai selezionato Territorio Urbano\n");
+    printf("inserire il numero di componenti della famiglia:\n");
+    scanf("%d", &comp);
+
+    printf("inserire il numero di metri quadrati della casa:\n");
+
+    scanf("%d", &qfmq);
 
     printf("Resoconto totale:\nComponenti della famiglia: %d\nMetri quadrati della casa:%d\n",comp,qfmq);
+        }
 
     return 0;
 }
