5e74ad9a614659b69278d15cfc1d28d2218e14d8
Francesco Masala <francescomasala270202@gmail.com>
2018-04-03 10:08:25 +0200

    Added Something
    :D

--- TIA.c
+++ TIA.c
@@ -12,13 +12,14 @@ int main() {
             int comp=0;
             int qfmq=0;
             int tariffa=0;
+            float quotavar=0;
+            float totale=0;
     printf("Calcolo per la Tariffia Igiene Ambientale\nVersione 0.1\nFrancesco Masala\n");
-    printf("Selezionare il territorio\nTerritorio Urbano=1\nTerritorio ExtraUrbano=2");
+    printf("Selezionare il territorio\nTerritorio Urbano=1\nTerritorio ExtraUrbano=2\n");
     scanf("%d", &tariffa);
 
-        if (tariffa == 2) {
-            printf("Hai selezionato Territorio Extraurbano\n");
-        } else {
+
+    if (tariffa == 2) {
             printf("Hai selezionato Territorio Extraurbano\n");
             printf("inserire il numero di componenti della famiglia:\n");
             scanf("%d", &comp);
@@ -28,18 +29,55 @@ int main() {
             scanf("%d", &qfmq);
 
             printf("Resoconto totale:\nComponenti della famiglia: %d\nMetri quadrati della casa:%d\n",comp,qfmq);
-        }
-        if (tariffa == 1) {
+
+
+    } else {
+
+            printf("hai selezionato Territorio Urbano\n");
+            printf("inserire il numero di componenti della famiglia:\n");
+            scanf("%d", &comp);
+
+            printf("inserire il numero di metri quadrati della casa:\n");
+
+            scanf("%d", &qfmq);
+
+            printf("Resoconto totale:\nComponenti della famiglia: %d\nMetri quadrati della casa:%d\n",comp,qfmq);
+        if(comp==1) {
+            quotavar=0.7313;
+            totale=quotavar*qfmq;
+                printf("Il totale dovuto da pagare è di: %f", totale);
+
         } else {
-            printf("Hai selezionato Territorio Urbano\n");
-    printf("inserire il numero di componenti della famiglia:\n");
-    scanf("%d", &comp);
+                    if(comp==2)  {
+                            quotavar=0.8594;
+                            totale=quotavar*qfmq;
+                            printf("Il totale dovuto da pagare è di: %f", totale);
 
-    printf("inserire il numero di metri quadrati della casa:\n");
+                    } else {
+                                    if(comp==3) {
+                                            quotavar=0.9599;
+                                            totale=quotavar*qfmq;
+                                            printf("Il totale dovuto da pagare è di: %f", totale);
+                                    } else {
+                                                if(comp==4) {
+                                                    quotavar=1.0423;
+                                                    totale=quotavar*qfmq;
+                                                    printf("Il totale dovuto da pagare è di: %f", totale);
+                                                } else {
+                                                        if(comp==5) {
+                                                                        quotavar=1.1246;
+                                                                        totale=quotavar*qfmq;
+                                                                        printf("Il totale dovuto da pagare è di: %f", totale);
+                                                                } else {
+                                                                        quotavar=1.1886;
+                                                                        totale=quotavar*qfmq;
+                                                                        printf("Il totale dovuto da pagare è di: %f", totale);
 
-    scanf("%d", &qfmq);
-
-    printf("Resoconto totale:\nComponenti della famiglia: %d\nMetri quadrati della casa:%d\n",comp,qfmq);
+                                                                 }
+                                                        }
+                                                }
+                                    }
+                    }
         }
 
     return 0;
